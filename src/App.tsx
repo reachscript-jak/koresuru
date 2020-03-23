d1306882e08d88f33c86e8f015ee02a366bc09ae
Takumi Okada <t_okada@freelance-jak.com>
2020-03-23 22:37:15 +0900

    サンプルボタン作成(自動デプロイ確認)

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const onClickGetData = () => alert('データ取得を押しました');
+  const onClickRegistData = () => alert('データ登録を押しました');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>コレスル</p>
+        <button onClick={onClickGetData}>データ取得</button>
+        <br />
+        <button onClick={onClickRegistData}>データ登録</button>
       </header>
     </div>
   );
